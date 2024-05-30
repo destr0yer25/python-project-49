401482c2f3b12233777de40aca47e16e90e3f724
destr0yer25 <rom.nikoloff@yandex.ru>
2024-05-31 00:26:12 +0300

    Fix progression_game_logic

--- brain_games/games/brain_progression_game_logic.py
+++ brain_games/games/brain_progression_game_logic.py
@@ -19,28 +19,33 @@ def creating_a_progression_series():
     return progression_series, first_number, step
 
 
+def is_the_progression_number(progress_list, starting_number, step):
+    flag = True
+    index_hidden_number = progress_list.index(random.choice(progress_list))
+    hidden_number = progress_list[index_hidden_number]
+    progress_list[index_hidden_number] = '..'
+    print(f'Question: {" ".join(progress_list)}')
+    user_ans = prompt.integer('Your answer: ')
+    if index_hidden_number == 0 and user_ans == starting_number:
+        print('Correct!')
+    elif (index_hidden_number <= len(progress_list)
+          - 1 and user_ans == int(progress_list[index_hidden_number - 1])
+          + step):
+        print('Correct!')
+    else:
+        flag = False
+        print(f"'{user_ans}' is wrong answer ;(."
+              f"Correct answer was '{hidden_number}'.")
+        print(f"Let's try again, {common_logic_bg.return_user_name()}!")
+    return flag
+
+
 def block_of_the_game_progression():
     print('What number is missing in the progression?')
-    flag = True
     for _ in range(3):
         progress_list, starting_number, step = creating_a_progression_series()
-        index_hidden_number = progress_list.index(random.choice(progress_list))
-        hidden_number = progress_list[index_hidden_number]
-        progress_list[index_hidden_number] = '..'
-        print(f'Question: {" ".join(progress_list)}')
-        user_ans = prompt.integer('Your answer: ')
-        if index_hidden_number == 0 and user_ans == starting_number:
-            print('Correct!')
-        elif (index_hidden_number <= len(progress_list)
-              - 1 and user_ans == int(progress_list[index_hidden_number - 1])
-              + step):
-            print('Correct!')
-        else:
-            flag = False
-            print(f"'{user_ans}' is wrong answer ;(."
-                  f"Correct answer was '{hidden_number}'.")
-            print(f"Let's try again, {common_logic_bg.return_user_name()}!")
+        flag = is_the_progression_number(progress_list, starting_number, step)
+        if flag is False:
             break
-    print(
-        f'Congratulations, {common_logic_bg.return_user_name()}!'
-    ) if flag else None
+    if flag:
+        print(f'Congratulations, {common_logic_bg.return_user_name()}!')
